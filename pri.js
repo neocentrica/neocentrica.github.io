155f00d3f9f112d2baaf238a9a4814437a33ca6b
neocentrica <64868274+neocentrica@users.noreply.github.com>
2022-04-26 00:57:25 -0500

    Update pri.js

--- pri.js
+++ pri.js
@@ -10,3 +10,39 @@ tabs.forEach(tab => {
     target.classList.add('active')
   })
 })
+
+function who() {
+  var x = document.getElementById("who");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
+
+function que() {
+  var x = document.getElementById("que");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function vis() {
+  var x = document.getElementById("vis");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function val() {
+  var x = document.getElementById("val");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
